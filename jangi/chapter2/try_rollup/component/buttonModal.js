6fac0528f39aa822d881b060fdd6013ee3e15899
dowookims <kaei2574@gmail.com>
2020-12-04 22:19:13 +0900

    [ADD] rollup & diary

---
+++ jangi/chapter2/try_rollup/component/buttonModal.js
@@ -0,0 +1,52 @@
+export default class ButtonModal {
+    constructor(openMessage, clickFn) {
+        this.openMessage = openMessage;
+        this.clickFn = clickFn;
+        this.preRender();
+    }
+
+    preRender() {
+        const content = this.htmlTemplate();
+        this.dom = document.createElement('div');
+        this.dom.className = 'button-modal-shell';
+        this.dom.innerHTML = content;
+        this.messageDom = this.dom.querySelector('.button-modal-content');
+        this.messageDom.innerText = this.openMessage;
+        this.yesBtn = this.dom.querySelector('.button-modal-yes');
+        this.noBtn = this.dom.querySelector('.button-modal-no');
+    }
+
+    render() {
+        this.bindEvent();
+        document.body.append(this.dom);
+    }
+
+    bindEvent() {
+        this.noBtn.addEventListener('click', () => {
+            this.dom.remove();
+        })
+        this.yesBtn.addEventListener('click', () => {
+            this.clickFn();
+            this.dom.remove();
+        });
+    }
+
+    htmlTemplate() {
+        return `
+            <div class="button-modal-container">
+                <div class="button-modal">
+                    <div class="button-modal-content">
+                    </div>
+                    <div class="button-modal-buttons">
+                        <button class="button-modal-yes">
+                            예
+                        </button>
+                        <button class="button-modal-no">
+                            아니오
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+}
---
+++ jangi/chapter2/try_rollup/component/line.js
@@ -0,0 +1,14 @@
+export default class Line {
+    constructor(coord, className='') {
+        this.coord = coord;
+        this.dom = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        this.dom.classList.add(className);
+    }
+
+    draw(parent) {
+        for (let key in this.coord) {
+            this.dom.setAttributeNS(null, key, this.coord[key]);
+        };
+        parent.append(this.dom);
+    }
+}
